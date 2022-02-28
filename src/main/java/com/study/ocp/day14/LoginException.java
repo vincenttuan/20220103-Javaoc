95f7991012d5039b0842659025944f95293203a4
teacher <teacher@192.168.1.180>
2022-02-28 20:38:27 +0800

    ocp

---
+++ src/main/java/com/study/ocp/day14/LoginException.java
@@ -0,0 +1,18 @@
+package com.study.ocp.day14;
+
+// 自定義例外錯誤類別需要繼承一個基礎例外(一般都是指: Exception, 最好是受檢例外)
+public class LoginException extends Exception {
+	private String errorMessage;
+	public LoginException(String errorMessage) {
+		super(errorMessage);
+		this.errorMessage = errorMessage;
+	}
+	
+	public void 要怎麼辦() {
+		System.out.println("請重新登入");
+	}
+	
+	public void 列印錯誤訊息() {
+		System.out.println("錯誤訊息:" + errorMessage);
+	}
+}
---
+++ src/main/java/com/study/ocp/day14/ThrowExceptionDemo3.java
@@ -0,0 +1,44 @@
+package com.study.ocp.day14;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+// 自訂例外類別
+public class ThrowExceptionDemo3 {
+	private static List<String> users = Arrays.asList("a01", "b02", "c03");
+	
+	public static void main(String[] args) {
+		try {
+			loginForm();
+		} catch (Exception e) {
+			System.out.println("錯誤訊息:" + e);
+			System.out.println("請重新登入");
+			main(null); // 重新執行 main 
+		}
+	}
+	
+	// 登入作業輸入介面
+	public static void loginForm() throws Exception {
+		System.out.print("請輸入 username: ");
+		Scanner sc = new Scanner(System.in);
+		String username = sc.next();
+		boolean check = checkLogin(username);
+		System.out.println("登入成功: " +check);
+	}
+	
+	// 驗證是否登入成功 ?
+	// 若驗證成功回傳 true, 反之拋出 Exception 例外
+	public static boolean checkLogin(String username) throws Exception {
+		Optional<String> opt = users.stream().filter(user -> user.equals(username)).findAny();
+		if(opt.isPresent()) {
+			return true;
+		} else {
+			Exception e = new Exception("找不到此人:" + username);
+			throw e;
+		}
+	}
+	
+	
+}
