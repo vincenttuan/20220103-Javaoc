bf58da4f2d2e5a37a43caf36d104399fcb82b560
teacher <teacher@192.168.1.180>
2022-03-14 20:06:22 +0800

    ocp

--- src/main/java/com/study/ocp/day18/MyLottoThreadPoolMain.java
+++ src/main/java/com/study/ocp/day18/MyLottoThreadPoolMain.java
@@ -1,6 +1,5 @@
 package com.study.ocp.day18;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -10,7 +9,7 @@ public class MyLottoThreadPoolMain {
 	public static void main(String[] args) throws Exception {
 		ThreadPoolExecutor exec = (ThreadPoolExecutor)Executors.newCachedThreadPool();
 		
-		Future t1 = exec.submit(new Lotto2());
+		Future<Integer> t1 = exec.submit(new Lotto2());
 		System.out.println(t1.get());
 		exec.submit(new Lotto());
 		exec.submit(new Lotto());
