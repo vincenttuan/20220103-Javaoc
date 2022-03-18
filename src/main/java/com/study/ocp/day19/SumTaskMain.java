4cedb363997da25dc4aa361769f01ba81b0c5c14
teacher <teacher@192.168.1.180>
2022-03-18 19:58:35 +0800

    ocp

---
+++ src/main/java/com/study/ocp/day19/SumTaskMain.java
@@ -0,0 +1,33 @@
+package com.study.ocp.day19;
+
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+
+public class SumTaskMain {
+
+	public static void main(String[] args) {
+		int len = 10;
+		long[] array = new long[len];
+		// 期望結果
+		long expectedSum = 0;
+		for(int i=0; i< array.length;i++) {
+			array[i] = i + 1;
+			expectedSum += array[i];
+		}
+		System.out.printf("任務: %s\n", Arrays.toString(array));
+		System.out.printf("期望結果: %d\n", expectedSum);
+		System.out.println("-------------------------------------------");
+		// ForkJoin task
+		ForkJoinTask<Long> task = new SumTask(array, 0, array.length);
+		// start time:
+		long startTime = System.currentTimeMillis();
+		// ForkJoin task 運行
+		long result = ForkJoinPool.commonPool().invoke(task);
+		// end time:
+		long endTime = System.currentTimeMillis();
+		System.out.printf("Fork/Join 結果: %d in %d ms.\n", 
+				result, (endTime - startTime));
+	}
+
+}
