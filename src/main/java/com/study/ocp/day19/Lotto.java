63ff044d1e28ffac52517a886708a43a21739efd
teacher <teacher@192.168.1.180>
2022-03-18 18:50:36 +0800

    ocp

---
+++ src/main/java/com/study/ocp/day19/Lotto.java
@@ -0,0 +1,20 @@
+package com.study.ocp.day19;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class Lotto implements Callable<Map<String, Integer>> {
+	private String name;
+	public Lotto(String name ) {
+		this.name = name;
+	}
+	@Override
+	public Map<String, Integer> call() throws Exception {
+		Map<String, Integer> map = new HashMap<>();
+		map.put(name, new Random().nextInt(100));
+		return map;
+	}
+
+}
