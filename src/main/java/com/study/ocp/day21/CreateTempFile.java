e68d8c29ca6d88f24cfa8aa5f66eb43694fe4e47
teacher <teacher@192.168.1.180>
2022-03-21 21:38:02 +0800

    ocp

--- src/main/java/com/study/ocp/day21/CreateTempFile.java
+++ src/main/java/com/study/ocp/day21/CreateTempFile.java
@@ -7,11 +7,10 @@ public class CreateTempFile {
 
 	public static void main(String[] args) throws IOException {
 		File filesPath = new File("src/main/java/com/study/ocp/day21/files");
-		// 前綴, 後綴, File 物件
 		for(int i=0;i<=10;i++) {
+			// 前綴, 後綴, File 物件
 			File tempFile = File.createTempFile("temp", ".txt", filesPath);
 			System.out.println(tempFile.getAbsolutePath());
 		}
 	}
-
 }
