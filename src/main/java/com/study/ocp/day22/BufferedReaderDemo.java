cb6d73dc19ea90dcd0f8258c120ff0e44106f443
teacher <teacher@192.168.1.180>
2022-03-25 20:50:07 +0800

    ocp

---
+++ src/main/java/com/study/ocp/day22/BufferedReaderDemo.java
@@ -0,0 +1,24 @@
+package com.study.ocp.day22;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class BufferedReaderDemo {
+
+	public static void main(String[] args) {
+		String filePath = "src\\main\\java\\com\\study\\ocp\\day22\\files\\data.txt";
+		try(FileReader fr = new FileReader(filePath);
+			BufferedReader br = new BufferedReader(fr);) {
+			
+			String data;
+			while ((data = br.readLine()) != null) {
+				System.out.println(data);
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+}
