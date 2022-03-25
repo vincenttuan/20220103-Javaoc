a7b196fcc39f54626fae44194b7470995f3e9bf3
teacher <teacher@192.168.1.180>
2022-03-25 19:25:32 +0800

    ocp

--- src/main/java/com/study/ocp/day22/FileWriterDemo.java
+++ src/main/java/com/study/ocp/day22/FileWriterDemo.java
@@ -7,7 +7,7 @@ public class FileWriterDemo {
 
 	public static void main(String[] args) {
 		String filePath = "src\\main\\java\\com\\study\\ocp\\day22\\files\\foo.txt";
-		try(FileWriter fr = new FileWriter(filePath)) {
+		try(FileWriter fr = new FileWriter(filePath, false)) { // false 不保留之前檔案內資料(預設), true 保留之前檔案內資料
 			String data1 = "現在時間:" + new Date().toString();
 			String data2 = "\n";
 			String data3 = "Welcome!";
