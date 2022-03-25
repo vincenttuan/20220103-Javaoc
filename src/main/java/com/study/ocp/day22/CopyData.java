3850cc2f7ffbfb5761727d2e4e5a7585568b874b
teacher <teacher@192.168.1.180>
2022-03-25 19:47:30 +0800

    ocp

---
+++ src/main/java/com/study/ocp/day22/CopyData.java
@@ -0,0 +1,23 @@
+package com.study.ocp.day22;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class CopyData {
+
+	public static void main(String[] args) {
+		String sourPath = "src\\main\\java\\com\\study\\ocp\\day22\\files\\data.txt";
+		String destPath = "src\\main\\java\\com\\study\\ocp\\day22\\files\\data_backup.txt";
+		try(FileReader fr = new FileReader(sourPath);
+			FileWriter fw = new FileWriter(destPath);) {
+			char[] buffer = new char[1];
+			while (fr.read(buffer) != -1) {
+				fw.write(buffer);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Copy OK!");
+	}
+
+}
