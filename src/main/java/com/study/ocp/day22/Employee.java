9ea7887f0e9de5a09870039e1b818d364ccd50d6
teacher <teacher@192.168.1.180>
2022-03-25 21:18:55 +0800

    ocp

---
+++ src/main/java/com/study/ocp/day22/Employee.java
@@ -0,0 +1,35 @@
+package com.study.ocp.day22;
+
+import java.util.Random;
+
+public class Employee {
+	private String name;
+	private Integer salary;
+	
+	public Employee(String name) {
+		this.name = name;
+		this.salary = new Random().nextInt(100000);
+	}
+	
+	public Employee() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getSalary() {
+		return salary;
+	}
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", salary=" + salary + "]";
+	}
+	
+}
