abfccadd7618888a524ece2e0b31fce22f01049f
teacher <teacher@192.168.1.180>
2022-03-28 21:16:35 +0800

    ocp

--- src/main/java/com/study/ocp/day23/FindTop10.java
+++ src/main/java/com/study/ocp/day23/FindTop10.java
@@ -3,6 +3,7 @@ package com.study.ocp.day23;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class FindTop10 {
@@ -11,7 +12,11 @@ public class FindTop10 {
 		Path path = Paths.get("src\\main\\java\\com\\study\\ocp\\day23\\words.txt");
 		try(Stream<String> lines = Files.lines(path)) {
 			
-			// your code here ...
+			lines.filter(w -> w.length() > 20)
+				 //.sorted((w1, w2) -> w2.length() - w1.length())
+				 .sorted(Comparator.comparingInt(String::length).reversed())	
+				 .limit(10)
+				 .forEach(w -> System.out.printf("%s (%d)%n", w, w.length()));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
