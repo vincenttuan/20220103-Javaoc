d93a2faf60298e8cad3083ac3fc37278dc02bd5e
teacher <teacher@192.168.1.180>
2022-03-28 21:39:52 +0800

    ocp

--- src/main/java/com/study/ocp/day23/FindWordCountByLength.java
+++ src/main/java/com/study/ocp/day23/FindWordCountByLength.java
@@ -4,6 +4,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FindWordCountByLength {
@@ -12,11 +14,26 @@ public class FindWordCountByLength {
 		Path path = Paths.get("src\\main\\java\\com\\study\\ocp\\day23\\words.txt");
 		try(Stream<String> lines = Files.lines(path)) {
 			
-			// your code here
+			lines.collect(Collectors.groupingBy(String::length, Collectors.counting()))
+				 .forEach((k, v) -> System.out.println(k + ":" + v));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		System.out.println("數量由大到小排序");
+		try(Stream<String> lines = Files.lines(path)) {
+			
+			lines.collect(Collectors.groupingBy(String::length, Collectors.counting()))
+				 .entrySet()
+				 .stream()
+				 //.sorted((a, b) -> (int)(b.getValue() - a.getValue()))
+				 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+				 .forEach(System.out::println);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
