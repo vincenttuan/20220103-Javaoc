5da0339c1de65c1276d4204c448e05beb3f746f6
teacher <teacher@192.168.1.180>
2022-04-01 20:05:18 +0800

    ocp

--- src/main/java/com/study/ocp/day24/console/SimpleClient.java
+++ src/main/java/com/study/ocp/day24/console/SimpleClient.java
@@ -1,5 +1,7 @@
 package com.study.ocp.day24.console;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -14,8 +16,20 @@ public class SimpleClient {
 		// 1. 建立 Socket 連線
 		Socket socket = new Socket(ip, port);
 		System.out.println("與 Server 端建立連線成功");
-		
-		// 2. 關閉 Socket 連線
+		// 2. 建立 Input/Output Stream
+		ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+		ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+		while (true) {
+			// 3. 將資料傳給 Server
+			System.out.print("請輸入傳送訊息:");
+			String message = sc.next();
+			if(message.equalsIgnoreCase("exit")) {
+				break;
+			}
+			// 4. 讀取 Server 傳來的資料
+			System.out.printf("Server 說: %s\n", (String)ois.readObject());
+		}
+		// 5. 關閉 Socket 連線
 		socket.close();
 	}
 
