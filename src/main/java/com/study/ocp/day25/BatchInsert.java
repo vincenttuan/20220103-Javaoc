5f874f234955425a18ec50a025c66a922a795ffa
teacher <teacher@BE-403->
2022-04-08 21:39:22 +0800

    ocp

---
+++ src/main/java/com/study/ocp/day25/BatchInsert.java
@@ -0,0 +1,38 @@
+package com.study.ocp.day25;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Arrays;
+import java.util.Random;
+
+import com.github.javafaker.Faker;
+
+// 批次新增
+public class BatchInsert {
+
+	public static void main(String[] args) {
+		String sql = "INSERT INTO Employee(name, age, salary) VALUES(?, ?, ?)";
+		String dbUrl = "jdbc:sqlite:src/main/java/com/study/ocp/day25/demo.db";
+		Random random = new Random();
+		Faker faker = new Faker();
+		try(Connection conn = DriverManager.getConnection(dbUrl);
+			PreparedStatement pstmt = conn.prepareStatement(sql);) {
+			pstmt.clearBatch(); // 清除 batch
+			
+			for(int i=0;i<10000;i++) {
+				pstmt.setString(1, faker.name().firstName());
+				pstmt.setInt(2, 20 + random.nextInt(20));
+				pstmt.setDouble(3, 25250 + random.nextInt(100000));
+				pstmt.addBatch();
+			}
+			
+			int[] rowcounts = pstmt.executeBatch(); // 批次執行
+			System.out.println(Arrays.toString(rowcounts));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
