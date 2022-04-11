c6c16e3f97913b5c01400e0bec93e4df7bb5cc5a
teacher <teacher@BE-403->
2022-04-11 19:31:55 +0800

    ocp

--- src/main/java/com/study/ocp/day26/CreateTables.java
+++ src/main/java/com/study/ocp/day26/CreateTables.java
@@ -4,30 +4,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.Arrays;
 
 public class CreateTables {
 
 	public static void main(String[] args) throws Exception {
 		String create_classify_table_sql = "create table classify( "
-				+ "id integer primary autoincrement, "
-				+ "name varchar(50) "
+				+ "id integer not null unique, "
+				+ "name varchar(50), "
+				+ "PRIMARY KEY(id)"
 				+ ")";
 		
 		String create_stock_table_sql = "create table stock( "
-				+ "id integer primary autoincrement, "
+				+ "id integer not null unique, "
 				+ "symbol varchar(50), "
 				+ "name varchar(50), "
 				+ "price real, "
 				+ "classify_id integer, "
-				+ "constraint fk_classify_id foreign key (classify_id) references classify(id) "
+				+ "constraint fk_classify_id foreign key (classify_id) references classify(id), "
+				+ "PRIMARY KEY(id)"
 				+ ")";
 		
 		String create_portfolio_table_sql = "create table portfolio( "
-				+ "id integer primary autoincrement, "
+				+ "id integer not null unique, "
 				+ "symbol varchar(50), "
 				+ "shares integer, "
 				+ "cost real,"
-				+ "constraint fk_symbol foreign key (symbol) references stock(symbol) "
+				+ "constraint fk_symbol foreign key (symbol) references stock(symbol), "
+				+ "PRIMARY KEY(id)"
 				+ ")";
 		
 		String dbUrl = "jdbc:sqlite:src/main/java/com/study/ocp/day25/demo.db";
@@ -38,7 +42,7 @@ public class CreateTables {
 		stmt.addBatch(create_stock_table_sql);
 		stmt.addBatch(create_portfolio_table_sql);
 		int[] rowcounts = stmt.executeBatch();
-		System.out.println("Batch create tables, rowcounts = " + rowcounts);
+		System.out.println("Batch create tables, rowcounts = " + Arrays.toString(rowcounts));
 	}
 
 }
