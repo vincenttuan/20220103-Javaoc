daee1eaa149b1e7d6b252e10ea77b871a5ee9fea
teacher <teacher@BE-403->
2022-04-11 19:44:56 +0800

    ocp

--- src/main/java/com/study/ocp/day26/BatchInsert.java
+++ src/main/java/com/study/ocp/day26/BatchInsert.java
@@ -1,11 +1,25 @@
 package com.study.ocp.day26;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Arrays;
+
 public class BatchInsert {
 
 	public static void main(String[] args) throws Exception {
 		String[] names = {"水泥", "食品", "半導", "光電", "觀光", "金融"};
 		String dbUrl = "jdbc:sqlite:src/main/java/com/study/ocp/day26/demo.db";
-		
+		String sql = "insert into classify(name) values(?)";
+		Connection conn = DriverManager.getConnection(dbUrl);
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.clearBatch();
+		for(String name : names) {
+			pstmt.setString(1, name);
+			pstmt.addBatch();
+		}
+		int[] rowscounts = pstmt.executeBatch();
+		System.out.println("Classify batch insert, rowscounts = " + Arrays.toString(rowscounts));
 	}
 	
 
