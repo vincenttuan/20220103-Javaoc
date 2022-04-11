347e913274b39a9d6c6de9af47e96c9c05c54b58
teacher <teacher@BE-403->
2022-04-11 21:24:38 +0800

    ocp

--- src/main/java/com/study/ocp/day26/BatchUpdatePrice.java
+++ src/main/java/com/study/ocp/day26/BatchUpdatePrice.java
@@ -6,7 +6,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
 public class BatchUpdatePrice {
 	public static void main(String[] args) throws Exception {
@@ -23,7 +28,23 @@ public class BatchUpdatePrice {
 		System.out.println(symbols);
 		// 利用 java yahoo finance api 查詢股價資料
 		// https://financequotes-api.com/
+		// ArrayList 轉 String[]
+		//System.out.println(Arrays.toString(symbols.toArray(new String[0])));
+		Map<String, Stock> stocks = YahooFinance.get(symbols.toArray(new String[0])); // single request
+		// 檢視 stocks 裡的資料 1
+		System.out.println(stocks);
+		// 檢視 stocks 裡的資料
+		stocks.forEach((k, v) -> System.out.println(k + ", " + v.getQuote().getPrice()));
 		
-		
+		final String sql2 = "update stock set price = ? where symbol = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql2);
+		pstmt.clearBatch();
+		for(Stock stock : stocks.values()) {
+			pstmt.setDouble(1, stock.getQuote().getPrice().doubleValue());
+			pstmt.setString(2, stock.getSymbol());
+			pstmt.addBatch();
+		}
+		int[] rowcounts = pstmt.executeBatch();
+		System.out.println("Stock batch update price, rowcounts = " + Arrays.toString(rowcounts));
 	}
 }
