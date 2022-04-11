11d08fc7441c9e6be85fb4e9f7c3f53076ec2af6
teacher <teacher@BE-403->
2022-04-11 19:41:18 +0800

    ocp

---
+++ src/main/java/com/study/ocp/day26/BatchInsert.java
@@ -0,0 +1,12 @@
+package com.study.ocp.day26;
+
+public class BatchInsert {
+
+	public static void main(String[] args) throws Exception {
+		String[] names = {"水泥", "食品", "半導", "光電", "觀光", "金融"};
+		String dbUrl = "jdbc:sqlite:src/main/java/com/study/ocp/day26/demo.db";
+		
+	}
+	
+
+}
